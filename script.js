b21156fe4b82bc47805198217027cace6b83ef5d
relzinga7 <93522617+relzinga7@users.noreply.github.com>
2021-11-29 16:15:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+    let name = 'Roy';
+    let x = 7;
+    let y = 5;
+    let age = 'twentyseven';
+
+    console.log(name);
+
+    console.log(x+y);
+
+    console.log(x-y);
+
+    console.log(x*y);
+
+    console.log(x/y);
+
+    console.log(x%y);
+
+    console.log(age)
+
+
